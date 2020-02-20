866bb8f3cbecb9f99097aa099080014699c2a446
D'agle Anderson <dagle_life@hotmail.com>
2020-02-20 14:20:09 -0300

    implementação de view de cadastro de modelo

--- src/gui/ModeloListController.java
+++ src/gui/ModeloListController.java
@@ -1,18 +1,28 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import model.entities.Modelo;
 import model.services.ModeloService;
 
@@ -54,8 +64,10 @@ public class ModeloListController implements Initializable{
 		}
 
 		@FXML 
-		public void OnBtnNovoAction() {
-			System.out.println("novo Modelo");
+		public void OnBtnNovoAction(ActionEvent event) {
+			Stage parentStage = Utils.currentStage(event);
+			Modelo modelo = new Modelo();
+			createDialogForm(modelo, "/gui/ModeloForm.fxml", parentStage);
 		}
 		
 		@FXML 
@@ -72,6 +84,27 @@ public class ModeloListController implements Initializable{
 		public void OnBtnSairAction() {
 			System.out.println("sair Modelo");
 		}
+		
+		public void createDialogForm(Modelo obj,String absoluteName, Stage parentStage){
+			try {
+				FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+				Pane pane = loader.load();
+				
+				Stage dialogStage = new Stage();
+				dialogStage.setTitle("Dados de Modelo");
+				dialogStage.setScene(new Scene(pane));
+				dialogStage.setResizable(false);
+				dialogStage.initOwner(parentStage);
+				dialogStage.initModality(Modality.WINDOW_MODAL);
+				dialogStage.showAndWait(); // carregar form na tela
+				
+				
+			}catch (IOException e) {
+				 Alerts.showAlert("IOExeption", "Erro loading View", e.getMessage(), AlertType.ERROR );
+			}
+			
+		}
+		
 		
 		//atualizador de dados do banco para a View
 		public void updateTableView() {
